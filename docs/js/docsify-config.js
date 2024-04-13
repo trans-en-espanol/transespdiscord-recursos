3a80a569f229298d58a189f4ba49b8cd723eae33
Lucía <14921905-lmaio@users.noreply.gitlab.com>
2024-04-13 23:24:19 +0200

    refactor: organize javascript

---
+++ docs/js/docsify-config.js
@@ -0,0 +1,28 @@
+var repository = "https://github.com/trans-en-espanol/transespdiscord-recursos";
+
+window.$docsify = {
+    name: "Trans en Español - Recursos",
+    repo: repository,
+    logo: "assets/img/logo.webp",
+    homepage: "pages/inicio.md",
+    coverpage: false,
+    loadSidebar: "pages/_sidebar.md",
+    //autoHeader: true,
+    subMaxLevel: 3,
+    toc: {
+        scope: '.markdown-section',
+        headings: 'h1, h2, h3, h4, h5, h6',
+        title: 'Índice',
+    },
+    search: {
+        placeholder: "Escribe aquí para buscar",
+        noData: "Sin resultados."
+    },
+    plugins: [
+        EditOnGithubPlugin.create(
+            repository + "/tree/main/docs/",
+            null,
+            "<i class=\"fa-solid fa-file-pen\"></i> Editar esta página"
+        )
+    ]
+}
